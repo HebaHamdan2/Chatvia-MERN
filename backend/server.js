5a420fa240ea8eaef2c2d445948e3c3317b0168f
Heba <hebasaeedhamdan296@gmail.com>
2024-03-18 22:44:14 +0200

    Backend Server

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,9 @@ app.use(cookieParser())
 app.use("/auth",authRouter)
 app.use("/messages",messageRouter)
 app.use("/users",userRouter)
-app.use(express.static(path.join(__dirname,"../frontend/build")))
+app.use(express.static(path.join(__dirname,"/frontend/build")))
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "/index.html"));
+	res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
 });
 const PORT = process.env.PORT || 5000;
 server.listen(PORT,()=>{
